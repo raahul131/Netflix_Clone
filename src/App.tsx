54366183facdc6e13ce7e99d031f1b0f5ffe161f
rahul0131 <ryadav@hamropatro.com>
2024-03-26 18:46:29 +0545

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Authentication from "components/Authentication";
+import Browse from "components/Browse";
 
 const App: React.FC = () => {
-  return <>Netflix</>;
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Authentication />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
 };
 
 export default App;
