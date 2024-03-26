acd7d99848f8db3b1440777cdcd643883929b0d4
rahul0131 <ryadav@hamropatro.com>
2024-03-26 19:15:08 +0545

    added dotEnv file

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQvj715yYkGtrldpIK4mg_yVcCbTz3SQs",
-  authDomain: "netflixclone-89405.firebaseapp.com",
-  projectId: "netflixclone-89405",
-  storageBucket: "netflixclone-89405.appspot.com",
-  messagingSenderId: "365547765447",
-  appId: "1:365547765447:web:1c3116a8207afa3b69c8e2",
-  measurementId: "G-TL1H3FRM6H",
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
